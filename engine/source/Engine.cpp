66d74de2798537d27caab9d0485f94f240b7f897
Lucas Martins <lucasmartinsti@outlook.com>
2026-04-13 12:55:58 -0300

    Novas Classes
    Cria Classes InputManager, ShaderProgram e GraphicsAPI

--- engine/source/Engine.cpp
+++ engine/source/Engine.cpp
@@ -1,13 +1,67 @@
 #include "Engine.h"
 #include "Application.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
 
 namespace eng
 {
-	bool Engine::Init()
+	void keyCallback(GLFWwindow* window, int key, int, int action, int)
+	{
+		auto& inputManager = eng::Engine::GetInstance().GetInputManager();
+		if (action == GLFW_PRESS)
+		{
+			inputManager.SetKeyPressed(key, true);
+		}
+		else if (action == GLFW_RELEASE)
+		{
+			inputManager.SetKeyPressed(key, false);
+		}
+	}
+
+	Engine& Engine::GetInstance()
+	{
+		static Engine instance;
+		return instance;
+
+	}
+	bool Engine::Init(int width, int height)
 	{
 		// Safety check: making sure we have an Application set before initializing, otherwise it crashes
 		if (!m_application)
 		{
+			return false;
+		}
+
+		if (!glfwInit())
+		{
+			return false;
+		}
+
+		//Config of wich OpenGl version were using here
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+		//Window Parameter
+		m_window = glfwCreateWindow(width, height, "GameEngine", nullptr, nullptr);
+
+		//Checks window ok
+		if (m_window == nullptr)
+		{
+			std::cout << "Error creating window" << std::endl;
+			glfwTerminate();
+			return false;
+		}
+
+		glfwSetKeyCallback(m_window, keyCallback);
+
+		glfwMakeContextCurrent(m_window);
+
+		if (glewInit() != GLEW_OK)
+		{
+			glfwTerminate();
 			return false;
 		}
 
@@ -24,15 +78,17 @@ namespace eng
 		// The classic Game Loop starts here
 		m_lastTimePoint = std::chrono::high_resolution_clock::now();
 
-		while (!m_application->NeedsToBeClosed())
+		while (!glfwWindowShouldClose(m_window) && !m_application->NeedsToBeClosed())
 		{
-			// Calculates the time passed since the last frame (deltaTime) 
-			// This is crucial so physics and movement aren't tied to the framerate
-			auto now = std::chrono::high_resolution_clock::now();
+			glfwPollEvents();//Process events
+
+			auto now = std::chrono::high_resolution_clock::now();// Calculates the time passed since the last frame (deltaTime) 
 			float deltaTime = std::chrono::duration<float>(now - m_lastTimePoint).count();
 			m_lastTimePoint = now;
 
 			m_application->Update(deltaTime);
+
+			glfwSwapBuffers(m_window);//Handle the rendering swaping buffers	
 		}
 	}
 
@@ -43,6 +99,8 @@ namespace eng
 			m_application->Destroy();
 			// reset() destroys the object held by the unique_ptr and safely frees the memory
 			m_application.reset();
+			glfwTerminate();
+			m_window = nullptr;
 		}
 	}
 
@@ -57,4 +115,15 @@ namespace eng
 		// Returns a raw pointer just for viewing/usage, the engine still owns the actual object
 		return m_application.get();
 	}
+
+	InputManager& Engine::GetInputManager()
+	{
+		return m_inputManager;
+	}
+
+	GraphicsAPI& Engine::GetGraphicsAPI()
+	{
+		return m_graphicsAPI;
+	}
+
 }
--- engine/source/Engine.h
+++ engine/source/Engine.h
@@ -1,19 +1,34 @@
 #pragma once
+#include "input/InputManager.h"
+#include "graphics/GraphicsAPI.h"
 #include <memory>
 #include <chrono>
 
+struct GLFWwindow; 
 namespace eng
 {
 	class Application;
 	class Engine
 	{
 	public:
-		bool Init();
+		static Engine& GetInstance();
+
+	private:
+		Engine() = default;
+		Engine(const Engine&) = delete;
+		Engine(Engine&&) = delete;
+		Engine& operator = (const Engine) = delete;
+		Engine& operator = (Engine&&) = delete;
+
+	public:
+		bool Init(int width, int height);
 		void Run();
 		void Destroy();
 
 		void SetApplication(Application* app);
 		Application* GetApplication();
+		InputManager& GetInputManager();
+		GraphicsAPI& GetGraphicsAPI();
 
 	private:
 		// Using unique_ptr so the engine automatically manages the app's memory and avoids leaks
@@ -21,6 +36,10 @@ namespace eng
 
 		// Stores the exact time of the last frame so we can calculate deltaTime later
 		std::chrono::steady_clock::time_point m_lastTimePoint;
+
+		GLFWwindow* m_window = nullptr;
+		InputManager m_inputManager;
+		GraphicsAPI m_graphicsAPI; 
 
 	};
 }
